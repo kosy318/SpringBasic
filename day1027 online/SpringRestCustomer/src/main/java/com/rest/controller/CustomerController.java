a2e3fb802a528088af09874abbd9d5ae256a48a4
고수연 <kosy1782@gmail.com>
2022-10-27 23:21:08 +0900

    10-27 online

--- day1027 online/SpringRestCustomer/src/main/java/com/rest/controller/CustomerController.java
+++ day1027 online/SpringRestCustomer/src/main/java/com/rest/controller/CustomerController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mvc.service.CustomerService;
@@ -28,14 +31,34 @@ public class CustomerController {
 		return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
 	}
 	
-	@GetMapping(value="/customers2")
-	public List<Customer> selectAll2(){
-		return customerService.selectAll();
-	}
+//	@GetMapping(value="/customers2")
+//	public List<Customer> selectAll2(){
+//		return customerService.selectAll();
+//	}
 	
 	@GetMapping(value="/customers/{num}")
 	public Customer selectOne(@PathVariable String num){
 		return customerService.selectOne(num);
+	}
+
+	@PostMapping(value="/customers")
+	public ResponseEntity<?> insert(@RequestBody Customer customer){
+		customerService.insert(customer);
+		List<Customer> list = customerService.selectAll();
+		if(list != null && !list.isEmpty()) {
+			return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PutMapping(value="/customers")
+	public ResponseEntity<?> update(@RequestBody Customer customer){
+		customerService.update(customer);
+		List<Customer> list = customerService.selectAll();
+		if(list != null && !list.isEmpty()) {
+			return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
 	}
 	
 }
