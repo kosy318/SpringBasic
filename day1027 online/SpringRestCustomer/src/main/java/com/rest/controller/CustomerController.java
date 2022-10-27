f7eea7e80dfa32c6f893587e9e93d7f94c4ecafb
고수연 <kosy1782@gmail.com>
2022-10-27 17:50:31 +0900

    10-27

---
+++ day1027 online/SpringRestCustomer/src/main/java/com/rest/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.rest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mvc.service.CustomerService;
+import com.mvc.vo.Customer;
+
+// @ RestController: @Controller + @ResponseBody(응답을 java에서 json으로 바꿔줌)
+@RestController
+public class CustomerController {
+	
+	@Autowired
+	CustomerService customerService;
+
+	@GetMapping(value="/customers")
+	public ResponseEntity<?> selectAll(){
+		List<Customer> list = customerService.selectAll();
+		if(list != null && !list.isEmpty()) {
+			return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping(value="/customers2")
+	public List<Customer> selectAll2(){
+		return customerService.selectAll();
+	}
+	
+	@GetMapping(value="/customers/{num}")
+	public Customer selectOne(@PathVariable String num){
+		return customerService.selectOne(num);
+	}
+	
+}
